4c14ad6ee98179884816a11b67eba13737994255
Matthis Moyon <matthis.moyon@hotmail.com>
2020-11-13 22:30:26 +0100

    Implementing a first version of the video player - Integrating the iFrame - Trying to find a way to control the video player

--- src/app/video-view/video-view.component.ts
+++ src/app/video-view/video-view.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-view.component.scss']
 })
 export class VideoViewComponent implements OnInit {
+  videoPlayer;
+  videoRatio;
+  videoPlayerControl;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.videoPlayer = document.getElementById('videoPlayer');
+    this.videoRatio = 9 / 16;
+
+    this.setVideoHeight();
+    this.initVideo();
   }
 
+  private initVideo() {
+    //window.videoPlayerControl.loadVideoById('iHCE8SVxbBM', 5, 'large');
+  }
+
+  private setVideoHeight() {
+    console.log('resize happening');
+
+    const currentWidth = this.videoPlayer.offsetWidth;
+    const newHeight = Math.round(currentWidth * this.videoRatio) + 'px';
+    this.videoPlayer.style.height = newHeight;
+  }
 }
