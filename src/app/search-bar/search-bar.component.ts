b7137668eb85c807a12eb42d5edbb4d1483e7f7b
Matthis Moyon <matthis.moyon@hotmail.com>
2020-11-14 19:08:28 +0100

    Implementation of the searchBar

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -10,6 +10,12 @@ export class SearchBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  onEnter(userInput:string) {
+    console.log("user would like to play : " +userInput);
+
   }
 
 }
