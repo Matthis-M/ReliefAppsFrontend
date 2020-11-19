b70fad14dac5d5dcb72109b16a875c48e7dfc876
Matthis Moyon <matthis.moyon@hotmail.com>
2020-11-19 16:32:33 +0100

    Commented code

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -12,49 +12,66 @@ export class BookmarksComponent implements OnInit {
   private videoControl: VideoControlService;
   private databaseLink: DatabaseLinkService;
 
+  // Initial text for the "show bookmarks" button, it also serves as a condition
   public toggleButtonText = 'Show bookmarks';
+  // Label showing the current number of bookmarks in the database
   public numberOfBookmarks: number;
+  // List containing all the bookmarks for the template
   public bookmarksList = [];
 
   constructor(
     dataBaseLinkService: DatabaseLinkService,
     videoControlService: VideoControlService
   ) {
+    // Dependency injection to use both services
     this.databaseLink = dataBaseLinkService;
     this.videoControl = videoControlService;
 
+    // Observer that refreshes the bookmarks as soon as the value gets changed from an API call to the backend
     this.databaseLink.Bookmarks$.subscribe((newBookmarks) => {
       this.refreshBookmarks(newBookmarks);
     });
   }
 
   ngOnInit(): void {
+    // Initially gets the latest values from the database at initialization of the component
     this.databaseLink.fetchBookmarks();
   }
 
+  // Send an order to the service to load the link clicked by the user.
   userClick(videoUrl: string) {
     this.videoControl.loadVideo(videoUrl);
   }
 
+  // This function is bound to the user clicking on the "add to bookmarks button"
   addToBookmarks() {
+    // Get the current video's URL from the video service
     const currentVideoUrl = this.videoControl.getVideoSource();
+    // Ask the database service to save it
     this.databaseLink.addToBookmarks(currentVideoUrl);
   }
 
+  // Update the values of the private members to match the latest information received from the backend
   refreshBookmarks(newBookmarks: string[]) {
+    // Update the bookmarks counter
     this.numberOfBookmarks = newBookmarks.length;
+    // Update the bookmarks list
     this.bookmarksList = newBookmarks;
   }
 
+  // Toggle the bookmark list on click
   toggleBookmarks() {
     const bookmarksList = document.getElementById(this.bookmarksListSelector);
 
+    // Toggle the "open" class on the element for the CSS toggling effect to play
     bookmarksList.classList.toggle('open');
+    // Toggle the text on the button
     this.toggleButtonText === 'Show bookmarks'
       ? (this.toggleButtonText = 'Hide bookmarks')
       : (this.toggleButtonText = 'Show bookmarks');
   }
 
+  // Method to clear all the history and bookmarks data in the database
   clearAll() {
     const choice = window.confirm(
       'Do you really want to reset History and Bookmarks ? This action is irreversible'
--- src/app/video-control.service.ts
+++ src/app/video-control.service.ts
@@ -6,38 +6,56 @@ import { Subject } from 'rxjs';
   providedIn: 'root',
 })
 export class VideoControlService {
+  // Regex that allows to check that the URL is in a correct Youtube format :
+  // It includes http and https, with or without "www.", and no "embed" URL
   private youtubeUrlRegexp = new RegExp(
     '^https?:\\/\\/(w{3}\\.)?youtube\\.com\\/watch\\?v=([A-Za-z0-9_-]{11})'
   );
+  // Regex that allows the extraction of the videoId from the URL
   private videoIdRegexp = new RegExp('watch\\?v=([A-Za-z0-9_-]{11})');
+  // Default video to show on the player
   public defaultVideo = 'https://www.youtube.com/watch?v=AYRwF3SCalU';
+  // Contains the URL source of the video. It cannot be a simple string for XSS-security reasons
+  // It's also a subject that will be observed for the player to use the new value when it changes
   private videoSourceSource = new Subject<ɵSafeResourceUrl>();
+  // string holding the URL source of the currently playing video in the "watch?v=" format
   private videoSourceString = '';
 
   public videoSource$ = this.videoSourceSource.asObservable();
 
   constructor(private domSanitizer: DomSanitizer) {}
 
+  // Load a new video from the provided URL if it's correct
   public loadVideo(newVideoUrl: string) {
+    // Check the validity of the URL
     const videoUrlValidationResult = this.youtubeUrlRegexp.exec(newVideoUrl);
 
+    // If it's valid
     if (videoUrlValidationResult) {
+      // Update the string source
       this.videoSourceString = newVideoUrl;
 
+      // Get the video ID from the "watch?v=" formatted new URL
       const videoId = this.videoIdRegexp.exec(newVideoUrl)[1];
 
+      // Change the URL to an "embed" format so it can be used by the player
+      // Also use a sanitizer to certify the safety of the URL
       const safeNewVideoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(
         'http://www.youtube.com/embed/' + videoId
       );
 
+      // Update the value of the source and inform the listening subscribers
       this.videoSourceSource.next(safeNewVideoUrl);
     } else {
+      // If the URL was found to be invalid, ask the user to provid a valid one
       window.alert(
         "The chosen Youtube URL is invalid, please try again with a valid one.\nIt should be similar to this : 'https://www.youtube.com/watch?v=AYRwF3SCalU'"
       );
     }
   }
 
+  // Return the URL of the currently playing video, as a string and
+  // in a "watch?v=" format so it can be used by the other components
   public getVideoSource() {
     return this.videoSourceString;
   }
